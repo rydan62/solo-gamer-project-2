dfe740f3375eedf69bc28741c2f23dfccd52a336
rydan62 <rydancouch2008@gmail.com>
2025-09-22 13:29:42 -0700

    added code but lacks cam

--- My project/Assets/scripts/PayerCon.cs
+++ My project/Assets/scripts/PayerCon.cs
@@ -1,38 +1,87 @@
-using UnityEditor;
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections
-public class pPayerCon : MonoBehaviour
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
 {
-    [Header("movement")]
-    public float moveSpeed;
-    
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
+    Camera playerCam;
     Rigidbody rb;
+    Ray jumpRay;
+
+    float inputX;
+    float inputY;
+
+    public float speed = 5f;
+    public float jumpHeight = 10f;
+    public float jumpRayDistance = 1.1f;
+
+    public int health = 200;
+    public int maxHealth = 200;
+
+
     private void Start()
     {
+        jumpRay = new Ray(transform.position, -transform.up);
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        playerCam = Camera.main;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
-    void Update()
+    private void Update()
     {
-        MyInput();
-    }
-    private void MyInput()
-    {
-        verticalInput = Input.GetAxisRaw("Vertical");
-        horizontalInput = Input.GetAxisRaw("horizontal");
-    }
-    private void MovePlayer()
-    {
-        moveDirection = Orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * moveSpeed * moveSpeed * 10f, ForceMode.Force);
-    }
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
         
+        Quaternion playerRotation = Quaternion.identity;
+        playerRotation.y = playerCam.transform.rotation.y;
+        playerRotation.w = playerCam.transform.rotation.w;
+        transform.rotation = playerRotation;
+
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
+
+       
+        Vector3 tempMove = rb.linearVelocity;
+
+        tempMove.x = inputY * speed;
+        tempMove.z = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.forward) +
+                            (tempMove.y * transform.up) +
+                            (tempMove.z * transform.right);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+
+        inputX = InputAxis.x;
+        inputY = InputAxis.y;
+    }
+    public void Jump()
+    {
+        if (Physics.Raycast(jumpRay, jumpRayDistance))
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "trap")
+        {
+            health = 20 - health;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            health = 20 - health;
+        }
+    }
+    
+
 }
