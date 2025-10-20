8317f3ab9012078ef345d802a72646f4b1638855
rydan62 <rydancouch2008@gmail.com>
2025-10-20 12:47:48 -0700

    started mor code

---
+++ My project/Assets/scripts/all mighty ball of god.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class allmightyballofgod : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "proj")
+        {
+            
+        }
+    }
+}
